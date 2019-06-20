c24173c9cd68b459cce3b452359f279189106d1e
Daniel Åkesson <argilstudio@gmail.com>
2019-06-20 14:46:58 +0200

    First commit

---
+++ Screen_To_JPG/screenToJPGForm.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace Screen_To_JPG
+{
+    partial class screenToJPGForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(screenToJPGForm));
+            this._choosePathBut = new System.Windows.Forms.Button();
+            this._takeSnapBut = new System.Windows.Forms.Button();
+            this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this._contextActions = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.takeSnapshotToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openWindowToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.quitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this._prefix = new System.Windows.Forms.TextBox();
+            this._choosePathDir = new System.Windows.Forms.FolderBrowserDialog();
+            this._openFileDir = new System.Windows.Forms.Button();
+            this.openImageDirectoryToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this._contextActions.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // _choosePathBut
+            // 
+            this._choosePathBut.BackColor = System.Drawing.Color.Moccasin;
+            resources.ApplyResources(this._choosePathBut, "_choosePathBut");
+            this._choosePathBut.Name = "_choosePathBut";
+            this._choosePathBut.UseVisualStyleBackColor = false;
+            this._choosePathBut.Click += new System.EventHandler(this._choosePathBut_Click);
+            // 
+            // _takeSnapBut
+            // 
+            this._takeSnapBut.BackColor = System.Drawing.Color.Salmon;
+            resources.ApplyResources(this._takeSnapBut, "_takeSnapBut");
+            this._takeSnapBut.Name = "_takeSnapBut";
+            this._takeSnapBut.UseMnemonic = false;
+            this._takeSnapBut.UseVisualStyleBackColor = false;
+            this._takeSnapBut.Click += new System.EventHandler(this._takeSnapBut_Click);
+            // 
+            // notifyIcon
+            // 
+            resources.ApplyResources(this.notifyIcon, "notifyIcon");
+            this.notifyIcon.ContextMenuStrip = this._contextActions;
+            // 
+            // _contextActions
+            // 
+            this._contextActions.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.takeSnapshotToolStripMenuItem,
+            this.openWindowToolStripMenuItem,
+            this.openImageDirectoryToolStripMenuItem,
+            this.quitToolStripMenuItem});
+            this._contextActions.Name = "_contextActions";
+            resources.ApplyResources(this._contextActions, "_contextActions");
+            // 
+            // takeSnapshotToolStripMenuItem
+            // 
+            this.takeSnapshotToolStripMenuItem.Name = "takeSnapshotToolStripMenuItem";
+            resources.ApplyResources(this.takeSnapshotToolStripMenuItem, "takeSnapshotToolStripMenuItem");
+            this.takeSnapshotToolStripMenuItem.Click += new System.EventHandler(this.takeSnapshotToolStripMenuItem_Click);
+            // 
+            // openWindowToolStripMenuItem
+            // 
+            this.openWindowToolStripMenuItem.Name = "openWindowToolStripMenuItem";
+            resources.ApplyResources(this.openWindowToolStripMenuItem, "openWindowToolStripMenuItem");
+            this.openWindowToolStripMenuItem.Click += new System.EventHandler(this.openWindowToolStripMenuItem_Click);
+            // 
+            // quitToolStripMenuItem
+            // 
+            this.quitToolStripMenuItem.Name = "quitToolStripMenuItem";
+            resources.ApplyResources(this.quitToolStripMenuItem, "quitToolStripMenuItem");
+            this.quitToolStripMenuItem.Click += new System.EventHandler(this.quitToolStripMenuItem_Click);
+            // 
+            // _prefix
+            // 
+            resources.ApplyResources(this._prefix, "_prefix");
+            this._prefix.Name = "_prefix";
+            this._prefix.TextChanged += new System.EventHandler(this._prefix_TextChanged);
+            // 
+            // _choosePathDir
+            // 
+            resources.ApplyResources(this._choosePathDir, "_choosePathDir");
+            // 
+            // _openFileDir
+            // 
+            this._openFileDir.BackColor = System.Drawing.Color.Moccasin;
+            resources.ApplyResources(this._openFileDir, "_openFileDir");
+            this._openFileDir.Name = "_openFileDir";
+            this._openFileDir.UseVisualStyleBackColor = false;
+            this._openFileDir.Click += new System.EventHandler(this._openFileDir_Click);
+            // 
+            // openImageDirectoryToolStripMenuItem
+            // 
+            this.openImageDirectoryToolStripMenuItem.Name = "openImageDirectoryToolStripMenuItem";
+            resources.ApplyResources(this.openImageDirectoryToolStripMenuItem, "openImageDirectoryToolStripMenuItem");
+            this.openImageDirectoryToolStripMenuItem.Click += new System.EventHandler(this.openImageDirectoryToolStripMenuItem_Click);
+            // 
+            // screenToJPGForm
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this._openFileDir);
+            this.Controls.Add(this._prefix);
+            this.Controls.Add(this._takeSnapBut);
+            this.Controls.Add(this._choosePathBut);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "screenToJPGForm";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.screenToJPGForm_FormClosing);
+            this._contextActions.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button _choosePathBut;
+        private System.Windows.Forms.Button _takeSnapBut;
+        private System.Windows.Forms.NotifyIcon notifyIcon;
+        private System.Windows.Forms.TextBox _prefix;
+        private System.Windows.Forms.FolderBrowserDialog _choosePathDir;
+        private System.Windows.Forms.ContextMenuStrip _contextActions;
+        private System.Windows.Forms.ToolStripMenuItem takeSnapshotToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openWindowToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem quitToolStripMenuItem;
+        private System.Windows.Forms.Button _openFileDir;
+        private System.Windows.Forms.ToolStripMenuItem openImageDirectoryToolStripMenuItem;
+    }
+}
+
---
+++ Screen_To_JPG/screenToJPGForm.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Threading;
+
+namespace Screen_To_JPG
+{
+    public partial class screenToJPGForm : Form
+    {
+        private int suffixNumber = 0;
+        public screenToJPGForm()
+        {
+            InitializeComponent();
+            _choosePathDir.SelectedPath = Directory.GetCurrentDirectory();
+        }
+       
+        //Functions
+        private void TakeSnapshot()
+        {
+            Bitmap img = CaptureWindow();
+            //Store in diclocation(to get the correct number do fileexist for N up until it returns false store said value!!)
+            while(File.Exists(_choosePathDir.SelectedPath + "/" + _prefix.Text + suffixNumber.ToString("00000") + ".jpg"))
+                suffixNumber++;
+            img.Save(_choosePathDir.SelectedPath + "/" + _prefix.Text + suffixNumber.ToString("00000") + ".jpg", ImageFormat.Jpeg);
+            suffixNumber++;
+        }
+        private Bitmap CaptureWindow()
+        {
+            Bitmap bitmap = new Bitmap(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height);
+            Graphics graphics = Graphics.FromImage(bitmap as Image);
+            graphics.CopyFromScreen(0, 0, 0, 0, bitmap.Size);
+            return bitmap;
+        }
+        //Window actions
+        private void _prefix_TextChanged(object sender, EventArgs e)
+        {
+            if(_prefix.Text.Trim() == "")
+                _prefix.Text = "img";
+        }
+        private void screenToJPGForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            e.Cancel = true;
+            Hide();
+            WindowState = FormWindowState.Minimized;
+            notifyIcon.Visible = true;
+        }
+        private void _choosePathBut_Click(object sender, EventArgs e)
+        {
+            _choosePathDir.ShowDialog();
+            suffixNumber = 0;
+        }
+        private void _takeSnapBut_Click(object sender, EventArgs e)
+        {
+            Hide();
+            notifyIcon.Visible = false;
+            Thread.Sleep(150);
+            TakeSnapshot();
+            Show();
+            notifyIcon.Visible = true;
+        }
+        private void _openFileDir_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start(_choosePathDir.SelectedPath);
+        }
+        //Context menu actions
+        private void takeSnapshotToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            notifyIcon.Visible = false;
+            Thread.Sleep(50);
+            TakeSnapshot();
+            notifyIcon.Visible = true;
+        }
+        private void openWindowToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Show();
+            WindowState = FormWindowState.Normal;
+            notifyIcon.Visible = false;
+        }
+        private void openImageDirectoryToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start(_choosePathDir.SelectedPath);
+        }
+        private void quitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            notifyIcon.Visible = false;
+            notifyIcon.Dispose();
+            Close();
+            Application.Exit();
+            System.Environment.Exit(1);
+        }
+    }
+}
